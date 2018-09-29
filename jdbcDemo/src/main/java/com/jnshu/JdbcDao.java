6207da71c4f3a7b1d022c6c82d12ee931a1390f9
luboliang <42486704+luboliang@users.noreply.github.com>
2018-09-29 15:25:34 +0800

    Add files via upload

---
+++ jdbcDemo/src/main/java/com/jnshu/JdbcDao.java
@@ -0,0 +1,18 @@
+package com.jnshu;
+
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface JdbcDao {
+    long add(Student student);
+
+    boolean delete(long id) throws SQLException;
+
+    boolean update(Student student);
+
+    Student select(long id);
+
+    List<Student> findStudents();
+
+}
